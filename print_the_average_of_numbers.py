69d4199e0166c739b4a1e1eb1c37ed614cbf97f7
Jacuzzi <jheklayag@gmail.com>
2025-03-12 17:41:43 +0800

    Pseudocode

---
+++ print_the_average_of_numbers.py
@@ -0,0 +1,5 @@
+# Use a list and ask the user to input numbeers while in a loop
+# Break the loop if the input is invalid
+# Get the sum of all inputs
+# Divide by the number of inputs
+# Print the average
