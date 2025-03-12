f59320714637815f695f4239f34f359d4022dfcc
Jacuzzi <jheklayag@gmail.com>
2025-03-12 17:17:47 +0800

    Loop breaks if input is invalid and counts the numbers in the list

--- print_the_numbers_with_most_duplicates.py
+++ print_the_numbers_with_most_duplicates.py
@@ -8,4 +8,14 @@ while True:
         numbers.append(number)
 # Break the loop if the input is invalid
 # Count each number in the list
+except ValueError:
+        break  
+
+if numbers:
+    unique_numbers = list(set(numbers))  
+    max_count = 0
+    most_frequent = numbers[0]  
+
+    for num in unique_numbers:
+        count = numbers.count(num)
 # Store the most frequent number in the list and print
