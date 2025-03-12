d2fe0bb788f4063cccd866cd35db0744adb265c9
Jacuzzi <jheklayag@gmail.com>
2025-03-12 16:29:04 +0800

    Inputs have been appended to lists and added to sets to separate duplicates

--- print_the_numbers_with_duplicates_only.py
+++ print_the_numbers_with_duplicates_only.py
@@ -5,5 +5,10 @@ duplicates = set()
 for i in range(10):
     num = int(input("Enter a number: "))
 # Store the inputs into a list and count every item
+    numbers.append(num)
+    numbers.count(num)
 # Store the numbers with duplicates into a set
+for num in numbers:
+    if numbers.count(num) > 1:
+        duplicates.add(num)
 # Print the numbers
