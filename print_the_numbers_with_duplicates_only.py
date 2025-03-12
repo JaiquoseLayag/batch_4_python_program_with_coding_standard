6a68c8221af912eae09a12c26b53fddf400afbde
Jacuzzi <jheklayag@gmail.com>
2025-03-12 16:27:22 +0800

    User input for 10 numbers have been added

--- print_the_numbers_with_duplicates_only.py
+++ print_the_numbers_with_duplicates_only.py
@@ -1,4 +1,9 @@
 # Ask the user to input 10 numbers
+numbers = []
+duplicates = set()
+
+for i in range(10):
+    num = int(input("Enter a number: "))
 # Store the inputs into a list and count every item
 # Store the numbers with duplicates into a set
 # Print the numbers
