505175039012a7ee7b076806b9e211513e654b2e
Jacuzzi <jheklayag@gmail.com>
2025-03-12 17:31:04 +0800

    Loop breaks if input is invalid and prints the biggest number

--- print_the_biggest_number.py
+++ print_the_biggest_number.py
@@ -6,5 +6,11 @@ while True:
         numbers = input("Enter a number: ")
         num = int(numbers)
         entered_numbers.add(num)            
-# Store the numbers in a set and check which is the smallest
+# Store the numbers in a set and check which is the highest
 # Break the loop once the input is invalid and print the smallest number
+    except ValueError:
+        print("Invalid input. Exiting")
+        break
+
+if entered_numbers:
+    print("Smallest Number: " + str(max(entered_numbers)))
