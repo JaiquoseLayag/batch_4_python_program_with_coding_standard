5f928323a567c0f4e5968599def4b03c6e1e1e4a
Jacuzzi <jheklayag@gmail.com>
2025-03-12 17:13:42 +0800

    Pseudocode

---
+++ print_the_numbers_with_most_duplicates.py
@@ -0,0 +1,4 @@
+# Use a list and ask the user to input numbers while in a loop
+# Break the loop if the input is invalid
+# Count each number in the list
+# Store the most frequent number in the list and print
